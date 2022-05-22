7622b7564bcd3083ab311a1499901c69ffbabfba
nick_ct <lchunting97@gmail.com>
2022-05-22 22:33:02 +0800

    .

--- lib/views/calendar/calendar_page.dart
+++ lib/views/calendar/calendar_page.dart
@@ -38,7 +38,7 @@ class _CalendarPageState extends State<CalendarPage> {
           onDaySelected: (selectedDay, focusedDay) {
             setState(() {
               _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
+              _focusedDay = selectedDay;
             });
           },
           onPageChanged: (focusedDay) {
@@ -47,8 +47,11 @@ class _CalendarPageState extends State<CalendarPage> {
             });
           },
           eventLoader: (day) {
+            if (day.isAtSameMomentAs(DateTime(2022, 05, 25))) {
+              return [const Event('event')];
+            }
             if (day.weekday == DateTime.monday) {
-              return [Event('Cyclic event')];
+              return [const Event('Cyclic event')];
             }
 
             return [];
@@ -67,38 +70,3 @@ class Event {
   @override
   String toString() => title;
 }
-
-/// Example events.
-///
-/// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
-final kEvents = LinkedHashMap<DateTime, List<Event>>(
-  equals: isSameDay,
-  hashCode: getHashCode,
-)..addAll(_kEventSource);
-
-final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-    key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-    value: (item) => List.generate(item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-  ..addAll({
-    kToday: [
-      Event('Today\'s Event 1'),
-      Event('Today\'s Event 2'),
-    ],
-  });
-
-int getHashCode(DateTime key) {
-  return key.day * 1000000 + key.month * 10000 + key.year;
-}
-
-/// Returns a list of [DateTime] objects from [first] to [last], inclusive.
-List<DateTime> daysInRange(DateTime first, DateTime last) {
-  final dayCount = last.difference(first).inDays + 1;
-  return List.generate(
-    dayCount,
-    (index) => DateTime.utc(first.year, first.month, first.day + index),
-  );
-}
-
-final kToday = DateTime.now();
-final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
-final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
